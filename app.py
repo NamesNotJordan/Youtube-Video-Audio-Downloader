17671a118b47d7e5809571fe6c551857f10c74b6
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-06-04 17:53:05 +0200

    added checkbox to choose audio only download. The destination path i defined isn't working. Instead of the actual downloads folder, it creates  new folders in the root folder

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+DOWNLOAD_DEST = "~/Downloads"
 
 def video_download():
     try:
         youtube_link = link.get()
         yt_object = YouTube(youtube_link, on_progress_callback=on_progress)
-        video = yt_object.streams.get_highest_resolution()
+        if audio_only_checkbox.get() == "on":
+            video = yt_object.streams.get_audio_only()
+        else:
+            video = yt_object.streams.get_highest_resolution()
         title.configure(text=video.title)
-        video.download()
+        video.download(DOWNLOAD_DEST)
         finish_label.configure(text="Download Complete")
     except:
         finish_label.configure(text="Invalid link", text_color="red")
 
-def audio_download():
-    pass
 
 # updates the progress bar and percent while downloading a video
 def on_progress(stream, chunk, bytes_remaining):
@@ -57,12 +59,14 @@ progress_percent.pack()
 progress_bar = customtkinter.CTkProgressBar(app, width=400)
 progress_bar.pack(padx=10, pady=10)
 
-# buttons
+# Audio only Check
+audio_only_var = customtkinter.StringVar(value="off")
+audio_only_checkbox = customtkinter.CTkCheckBox(app, text="Audio Only", variable=audio_only_var, onvalue="on", offvalue="off")
+audio_only_checkbox.pack(padx=10, pady=10)
+# button
 video_download_button = customtkinter.CTkButton(app, text="Download", command=video_download)
 video_download_button.pack()
 
-download_audio_button = customtkinter.CTkButton(app, text="Download Audio Only", command= audio_download)
-download_audio_button.pack()
 
 
 # Run app
