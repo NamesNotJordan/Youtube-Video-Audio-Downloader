69533a34859960a21fc845c454ca03956676481d
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-06-04 16:38:01 +0200

    Created requirements list and started building GUI

---
+++ app.py
@@ -0,0 +1,13 @@
+# TODO: GUI
+# TODO: Download videos
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+# System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# App Frame
+app = customtkinter.CTk()
+app.geometry("720x480")
