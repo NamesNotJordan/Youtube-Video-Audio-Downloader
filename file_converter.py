5190cf9309a0e9da83fc5340c9a3fc6d32968d9b
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-09-27 19:48:07 +0200

    added destination folder argument. Allows the converted file to be placed somewhere other than the original file's location

--- file_converter.py
+++ file_converter.py
@@ -1,6 +1,6 @@
 import os
 
 
-def convert_to_mp3(file_name, file_destination):
+def convert_to_mp3(file_name,source_folder, destination_folder):
     new_file_name = file_name[:-3] + "mp3"
-    os.system(f"ffmpeg -i {os.path.join(file_destination,file_name) } {file_destination}/{new_file_name}")
+    os.system(f"ffmpeg -i {os.path.join(source_folder,file_name) } {destination_folder}/{new_file_name}")
