276542e4f397511e3cf628a286a1178aac526d38
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-09-27 20:02:13 +0200

    named batch converting function. Changed mp3 converter to a more versatile single file converter

--- file_converter.py
+++ file_converter.py
@@ -1,6 +1,11 @@
 import os
 
 
-def convert_to_mp3(file_name,source_folder, destination_folder):
-    new_file_name = file_name[:-3] + "mp3"
+def convert_single(file_name, source_folder, destination_folder, format:str):
+    #new_file_name = file_name[:-3] + f".{format}"
+    # Splits the file name from it's extension then adds the new extension
+    new_file_name = os.path.splitext(file_name)[0] + f".{format}"
     os.system(f"ffmpeg -i {os.path.join(source_folder,file_name) } {destination_folder}/{new_file_name}")
+
+def convert_batch(source_folder, destination_folder):
+    pass
